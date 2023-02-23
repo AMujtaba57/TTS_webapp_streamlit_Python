ec05bbaa6d782c43f4347c86ce86582a1e0f556f
Ahmad057 <ahmad.mujtuba@arhamsoft.org>
2023-02-23 14:46:23 +0500

    initial design pages

---
+++ pages/01_Twitter_Tracking_Service.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+st.write("<h3>Overview</h3>", unsafe_allow_html=True)
+
+st.write("######")
+st.write("######")
+st.write("######")
+st.write("######")
+st.write("######")
+
+st.write("######")
+st.write("######")
+st.write("######")
+
+st.write("<h3>About</h3>", unsafe_allow_html=True)
